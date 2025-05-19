8d7a19be9c29df6b403e9440873bf17da6035157
kimheejoo0828 <o1043381828@nate.com>
2025-05-19 16:28:31 +0900

    Player
    플레이어 이동 및 점프 마우스로 방향 구현

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static CharacterManager _instance;
+
+    public static CharacterManager Instance
     {
-        
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new GameObject("CharacterManager").AddComponent<CharacterManager>();
+            }
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Player _player;
+    public Player Player
     {
-        
+        get {  return _player; }
+        set { _player = value; }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if(_instance == this)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
